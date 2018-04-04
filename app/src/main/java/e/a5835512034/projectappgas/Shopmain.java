db98782222e7de44c648cb1308ad1fc9b10c77a8
samchampkittisak <5835512034@email.psu.ac.th>
2018-04-04 22:54:58 +0700

    save1

---
+++ app/src/main/java/e/a5835512034/projectappgas/Shopmain.java
@@ -0,0 +1,12 @@
+package e.a5835512034.projectappgas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Shopmain extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shop_main);
+    }
+}
